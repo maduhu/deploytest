789d7580092b36af32abe347527d56220c8e2524
BHCrosslake <billh@crosslaketech.com>
2016-08-22 06:55:14 -0700

    update copyright

--- copyright.js
+++ copyright.js
@@ -5,11 +5,7 @@
  * Example of nodejs module file comment header
  *
  * @copyright
- * NOTICE: This material is proprietary and confidential and developed for
- * the Bill & Melinda Gates Foundation Level One Project. Disclosure,
- * distribution, reproduction, or use of this material is strictly prohibited
- * without the prior written consent of the Bill & Melinda Gates Foundation.
- * 
- * Copyright © 2016, Bill & Melinda Gates Foundation. All Rights Reserved.
+ * Copyright Â© 2016, Bill & Melinda Gates Foundation. All Rights Reserved. Distribution,
+ * reproduction or use is prohibited without permission.
  */
 
